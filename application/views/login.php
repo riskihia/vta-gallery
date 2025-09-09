9adf4179e07ca9c78de9fdd51cb53852d1ffd2e7
riskihia <riskirahmathia3@gmail.com>
2025-09-09 13:47:20 +0700

    feat: change favicon on login page

--- application/views/login.php
+++ application/views/login.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?php global $config; echo $config['app_name']?></title>
-  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo BASE_URL ?>static/images/favicons.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo BASE_URL ?>static/images/vitechasia2.png">
   <!-- Global stylesheets -->
   <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
   <link href="<?php echo BASE_URL; ?>static/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
